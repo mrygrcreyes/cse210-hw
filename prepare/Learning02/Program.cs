e27310cfa2071f3270fe4cff31b3b6f541bf0d00
Grace Reyes <marygracereyes2315@gmail.com>
2023-04-26 14:18:37 +0800

    Finished Abstraction Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Web Developer";
+        job1._company = "Apple";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mary Grace";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+        // This method can be used but for shortcut you can display jobs in job.cs using (foreach)
+        // job1.Display();
+        // job2.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"NAME: {_name}");
+        Console.WriteLine("JOBS:");
+
+        // This is a shortcut/other method if there is 2 things to display
+
+        {
+        foreach (Job job in _jobs)
+        job.Display();
+        }
+
+
+    }
+}
