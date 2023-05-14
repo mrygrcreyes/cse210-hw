c010537c51f9f57a0a4523fd5372d249548f59a3
Grace Reyes <marygracereyes2315@gmail.com>
2023-05-14 10:05:18 +0800

    Finished

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,41 @@
+public class BreathingActivity : Activity
+{
+    public BreathingActivity(string chosenActivity) :base(chosenActivity)
+    {
+
+    }
+
+    public void DoBreath()
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(_chosenTime);
+        do
+        {
+            Console.Write("Breathe in...");
+            Console.Write("4");
+            Wait1Sec();
+            Console.Write("3");
+            Wait1Sec();
+            Console.Write("2");
+            Wait1Sec();
+            Console.Write("1");
+            Wait1Sec();
+            Console.WriteLine(" ");
+            Console.Write("Now breath out...");
+            Console.Write("6");
+            Wait1Sec();
+            Console.Write("5");
+            Wait1Sec();
+            Console.Write("4");
+            Wait1Sec();
+            Console.Write("3");
+            Wait1Sec();
+            Console.Write("2");
+            Wait1Sec();
+            Console.Write("1");
+            Wait1Sec();
+            Console.WriteLine(" ");
+            Console.WriteLine("");
+        }while(DateTime.Now < futureTime);
+    }
+}
